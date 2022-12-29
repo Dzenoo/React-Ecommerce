9ec6658cf717b62d3996c00a9995e1e6068de43f
Dzenis Gudzevic <110186379+Dzenoo@users.noreply.github.com>
2022-12-29 20:55:27 +0100

    Update index.js

--- frontend/src/shared/redux/index.js
+++ frontend/src/shared/redux/index.js
@@ -3,7 +3,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import AuthSlice from "./auth-slice";
 
 const store = configureStore({
-  reducer: { auth: AuthSlice.reducer },
+  reducer: { auth: AuthSlice.reducer, cart: CartSlice.reducer },
 });
 
 export default store;
