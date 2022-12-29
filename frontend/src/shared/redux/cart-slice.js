49a25e9873059c4597f267c88bea82c6d88c3754
Dzenis Gudzevic <110186379+Dzenoo@users.noreply.github.com>
2022-12-29 20:55:02 +0100

    Add files via upload

---
+++ frontend/src/shared/redux/cart-slice.js
@@ -0,0 +1,46 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const CartSlice = createSlice({
+  name: "cart",
+  initialState: { items: [], totalQuantity: 0 },
+  reducers: {
+    AddToCart(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.items.find((item) => item.id === newItem.id);
+      state.totalQuantity++;
+
+      if (!existingItem) {
+        state.items.push({
+          id: newItem.id,
+          image: newItem.image,
+          title: newItem.title,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+          option: newItem.option,
+        });
+      } else {
+        existingItem.quantity++;
+        existingItem.totalPrice =
+          parseInt(existingItem.totalPrice) + parseInt(newItem.price);
+      }
+    },
+
+    RemoveFromCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      state.totalQuantity--;
+
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice =
+          parseInt(existingItem.totalPrice) - parseInt(existingItem.price);
+      }
+    },
+  },
+});
+
+export const cartActions = CartSlice.actions;
+export default CartSlice;
