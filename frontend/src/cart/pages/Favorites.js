73516d57383c55ed0f6256a3c21f41401d0bf6aa
Dzenis Gudzevic <110186379+Dzenoo@users.noreply.github.com>
2022-12-31 16:35:34 +0100

    Add files via upload

---
+++ frontend/src/cart/pages/Favorites.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+import "./Favorites.css";
+
+import { useSelector } from "react-redux";
+import Card from "../../shared/components/UIelements/Card";
+
+import FavItem from "../components/FavItem";
+
+const Favorites = () => {
+  const favItems = useSelector((state) => state.favorite.favItems);
+
+  return (
+    <>
+      <div>
+        {favItems.length === 0 && (
+          <Card className="card">
+            <h1>Lista zelja je prazna</h1>
+          </Card>
+        )}
+        {favItems.length >= 1 &&
+          favItems.map((item) => (
+            <FavItem
+              id={item.id}
+              key={item.id}
+              image={item.image}
+              title={item.title}
+              description={item.description}
+            />
+          ))}
+      </div>
+    </>
+  );
+};
+
+export default Favorites;
