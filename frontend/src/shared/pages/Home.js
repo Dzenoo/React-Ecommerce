2eb253f56d1f72a2e035706304a2e415439803fa
Dzenis Gudzevic <110186379+Dzenoo@users.noreply.github.com>
2022-12-31 17:01:31 +0100

    Update Home.js

--- frontend/src/shared/pages/Home.js
+++ frontend/src/shared/pages/Home.js
@@ -3,7 +3,17 @@ import React from "react";
 import "./Home.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <div className="mainpage">
+        <h1>
+          <span className="ch">MOST</span> WANTED <br />
+          <span className="ch">AND</span> MOST
+          <span className="ch">LOVED</span>
+        </h1>
+      </div>
+    </>
+  );
 };
 
 export default Home;
