72909c4d25bd7b9af7b16c82329ab9ca0bd105e1
Dzenis Gudzevic <110186379+Dzenoo@users.noreply.github.com>
2023-01-04 17:15:02 +0100

    Update Auth.js

--- frontend/src/user/pages/Auth.js
+++ frontend/src/user/pages/Auth.js
@@ -1,20 +1,23 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { useForm } from "../../shared/hooks/form-hook";
+import { useHttpClient } from "../../shared/hooks/http-hook";
+import { AuthContext } from "../../shared/context/auth-context";
 import {
   VALIDATOR_EMAIL,
   VALIDATOR_MINLENGTH,
   VALIDATOR_REQUIRE,
 } from "../../shared/util/validate";
-import { AuthActions } from "../../shared/redux/auth-slice";
 import Button from "../../shared/components/Form/Button";
 import Input from "../../shared/components/Form/Input";
 import Card from "../../shared/components/UIelements/Card";
+import ErrorModal from "../../shared/components/UIelements/ErrorModal";
+import Loader from "../../shared/components/UIelements/Loader";
 import "./Auth.css";
-import { useDispatch } from "react-redux";
 
 const Auth = () => {
-  const [authMod, setAuthMod] = useState(false);
-  const dispatch = useDispatch();
+  const auth = useContext(AuthContext);
+  const [authMod, setAuthMod] = useState(true);
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
 
   const [formState, inputHandler, setFormData] = useForm(
     {
@@ -57,57 +60,93 @@ const Auth = () => {
     setAuthMod((prevMode) => !prevMode);
   };
 
-  const AuthSubmitHandler = (event) => {
+  const AuthSubmitHandler = async (event) => {
     event.preventDefault();
-    dispatch(AuthActions.login());
-    console.log(formState.inputs);
+
+    if (authMod) {
+      try {
+        const responseData = await sendRequest(
+          "http://localhost:8000/api/users/login",
+          "POST",
+          JSON.stringify({
+            email: formState.inputs.email.value,
+            password: formState.inputs.password.value,
+          }),
+          {
+            "Content-Type": "application/json",
+          }
+        );
+        auth.login(responseData.userId, responseData.token);
+      } catch (err) {}
+    } else {
+      try {
+        const responseData = await sendRequest(
+          "http://localhost:8000/api/users/signup",
+          "POST",
+          JSON.stringify({
+            name: formState.inputs.name.value,
+            email: formState.inputs.email.value,
+            password: formState.inputs.password.value,
+          }),
+          {
+            "Content-Type": "application/json",
+          }
+        );
+
+        auth.login(responseData.userId, responseData.token);
+      } catch (err) {}
+    }
   };
 
   return (
-    <Card className="form_card">
-      <h2>{authMod ? "Prijavi se" : "Registruj se "}</h2>
-      <hr />
-      <form className="auth_form" onSubmit={AuthSubmitHandler}>
-        {!authMod && (
+    <div className="form_div">
+      <ErrorModal error={error} onClear={clearError} />
+      <Card className="form_card">
+        {isLoading && <Loader asOverlay />}
+        <h2>{authMod ? "Prijavi se" : "Registruj se "}</h2>
+        <hr />
+        <form className="auth_form" onSubmit={AuthSubmitHandler}>
+          {!authMod && (
+            <Input
+              element="input"
+              id="name"
+              type="text"
+              label="Ime"
+              validators={[VALIDATOR_REQUIRE()]}
+              errorText="Unesite ispravno ime!"
+              onInput={inputHandler}
+            />
+          )}
           <Input
             element="input"
-            id="name"
-            type="text"
-            label="Ime"
-            validators={[VALIDATOR_REQUIRE()]}
-            errorText="Unesite ispravno ime!"
+            id="email"
+            type="email"
+            label="E-Mail"
+            validators={[VALIDATOR_EMAIL()]}
+            errorText="Unesite ispravan mejl!"
             onInput={inputHandler}
           />
-        )}
-        <Input
-          element="input"
-          id="email"
-          type="email"
-          label="E-Mail"
-          validators={[VALIDATOR_EMAIL()]}
-          errorText="Unesite ispravan mejl!"
-          onInput={inputHandler}
-        />
-        <Input
-          element="input"
-          id="password"
-          type="password"
-          label="Password"
-          validators={[VALIDATOR_MINLENGTH(6)]}
-          errorText="Unesite ispravnu sifru!"
-          onInput={inputHandler}
-        />
+          <Input
+            element="input"
+            id="password"
+            type="password"
+            label="Password"
+            validators={[VALIDATOR_MINLENGTH(6)]}
+            errorText="Unesite ispravnu sifru!"
+            onInput={inputHandler}
+          />
 
-        <div style={{ display: "flex", gap: "1em", alignItems: "center" }}>
-          <Button type="submit" disabled={!formState.isValid}>
-            {authMod ? "Prijavi se" : "Registruj se "}
-          </Button>
-          <a className="switchbtn" onClick={switchMode}>
-            {authMod ? "Registruj se" : "Prijavi se"}
-          </a>
-        </div>
-      </form>
-    </Card>
+          <div style={{ display: "flex", gap: "1em", alignItems: "center" }}>
+            <Button type="submit" disabled={!formState.isValid}>
+              {authMod ? "Prijavi se" : "Registruj se "}
+            </Button>
+            <a className="switchbtn" onClick={switchMode}>
+              {authMod ? "Registruj se" : "Prijavi se"}
+            </a>
+          </div>
+        </form>
+      </Card>
+    </div>
   );
 };
 
