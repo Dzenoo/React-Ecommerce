8bb48cdece06a5c9d4c784ab441a25e9fd28f497
Dzenis Gudzevic <110186379+Dzenoo@users.noreply.github.com>
2023-01-05 12:02:28 +0100

    Add files via upload

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,34 @@
+const fs = require("fs");
+const path = require("path");
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
 const productRoutes = require("./routes/product_routes.js");
 const userRoutes = require("./routes/user_routes");
+const orderRoutes = require("./routes/order_routes");
 const HttpError = require("./models/http-error.js");
 
 const app = express();
 
 app.use(bodyParser.json());
 
+app.use("/uploads/images", express.static(path.join("uploads", "images")));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*"); // Pristupi Api sa bilo kojeg servera, send Request
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  ); // Da navedemo koji headers mogu imati ovi requests od browsera
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE"); // Koje http metode se mogu koristiti u frontendu
+
+  next();
+});
+
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
 
 app.use((req, res, next) => {
   const error = new HttpError("Could not find this route", 404);
@@ -19,6 +36,12 @@ app.use((req, res, next) => {
 });
 
 app.use((error, req, res, next) => {
+  if (req.file) {
+    fs.unlink(req.file.path, (err) => {
+      console.log(err);
+    });
+  }
+
   if (res.headerSent) {
     return next(error);
   }
