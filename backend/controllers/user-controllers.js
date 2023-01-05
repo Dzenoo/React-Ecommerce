351816afa01a7209c648c298e36dbbb55fa34981
Dzenis Gudzevic <110186379+Dzenoo@users.noreply.github.com>
2023-01-05 11:57:35 +0100

    Add files via upload

--- backend/controllers/user-controllers.js
+++ backend/controllers/user-controllers.js
@@ -1,4 +1,6 @@
 const { validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const HttpError = require("../models/http-error");
 const User = require("../models/user");
@@ -20,10 +22,10 @@ exports.getUsers = async (req, res, next) => {
 exports.signup = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return next(new HttpError("Nevazeci unosi su prazni", 422));
+    return next(new HttpError("Nevazeci unosi", 422));
   }
 
-  const { name, email, password, isAdmin } = req.body;
+  const { name, email, password } = req.body;
 
   let existingUser;
   try {
@@ -38,11 +40,19 @@ exports.signup = async (req, res, next) => {
     return next(error);
   }
 
+  let hashedPassword;
+  try {
+    hashedPassword = await bcrypt.hash(password, 12);
+  } catch (err) {
+    const error = new HttpError("Korisnik se ne moze kreirati ", 500);
+    return next(error);
+  }
+
   const createdUser = new User({
     name,
     email,
-    password,
-    isAdmin,
+    password: hashedPassword,
+    isAdmin: false,
   });
 
   try {
@@ -55,31 +65,93 @@ exports.signup = async (req, res, next) => {
     return next(error);
   }
 
-  res.status(201).json({ user: createdUser.toObject({ getters: true }) });
+  let token;
+  try {
+    token = jwt.sign(
+      {
+        userId: createdUser.id,
+        email: createdUser.email,
+        name: createdUser.name,
+        isAdmin: createdUser.isAdmin,
+      },
+      process.env.JWTSTR,
+      { expiresIn: "1h" }
+    );
+  } catch (err) {
+    const error = new HttpError(
+      "Registrovanje neuspesno, probajte ponovo",
+      500
+    );
+    return next(error);
+  }
+
+  res
+    .status(201)
+    .json({ userId: createdUser.id, email: createdUser.email, token: token });
 };
 
 exports.login = async (req, res, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return next(
-      new HttpError("Netacni mail ili lozinka , proverite svoje podatke", 422)
-    );
-  }
-
   const { email, password } = req.body;
 
   let existingUser;
   try {
     existingUser = await User.findOne({ email: email });
   } catch (err) {
-    const error = new HttpError("Ulogovanje neuspesno", 500);
+    const error = new HttpError("Prijavljivanje neuspesno", 500);
     return next(error);
   }
 
-  if (!existingUser || existingUser.password !== password) {
-    const error = new HttpError("Ulogovanje neuspesno, netacni podaci", 500);
+  if (!existingUser) {
+    const error = new HttpError(
+      "Prijavljivanje neuspesno, netacni podaci",
+      500
+    );
     return next(error);
   }
 
-  res.json({ message: "Uspesno Ulogovani" });
+  let isValidPassword = false;
+  try {
+    isValidPassword = await bcrypt.compare(password, existingUser.password);
+  } catch (err) {
+    const error = new HttpError(
+      "Prijavljivanje neuspesno, netacni podaci",
+      500
+    );
+    return next(error);
+  }
+
+  if (!isValidPassword) {
+    const error = new HttpError(
+      "Prijavljivanje neuspesno, netacni podaci",
+      500
+    );
+    return next(error);
+  }
+
+  let token;
+  try {
+    token = jwt.sign(
+      {
+        userId: existingUser.id,
+        email: existingUser.email,
+        name: existingUser.name,
+        isAdmin: existingUser.isAdmin,
+      },
+      process.env.JWTSTR,
+      { expiresIn: "1h" }
+    );
+  } catch (err) {
+    const error = new HttpError(
+      "Prijavljivanje neuspesno, probajte ponovo",
+      500
+    );
+    return next(error);
+  }
+
+  res.json({
+    userId: existingUser.id,
+    email: existingUser.email,
+    isAdmin: existingUser.isAdmin,
+    token: token,
+  });
 };
