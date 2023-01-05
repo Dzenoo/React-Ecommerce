800f6616eb9ea689fc2b122bf9b4d8663b612004
Dzenis Gudzevic <110186379+Dzenoo@users.noreply.github.com>
2023-01-05 12:03:28 +0100

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -16,12 +16,12 @@ app.use(bodyParser.json());
 app.use("/uploads/images", express.static(path.join("uploads", "images")));
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*"); // Pristupi Api sa bilo kojeg servera, send Request
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  ); // Da navedemo koji headers mogu imati ovi requests od browsera
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE"); // Koje http metode se mogu koristiti u frontendu
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
 
   next();
 });
