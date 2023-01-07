4df60954e8afacd601b78334f88c99242be3aa24
Dzenis Gudzevic <110186379+Dzenoo@users.noreply.github.com>
2023-01-07 12:05:01 +0100

    Update index.js

--- frontend/src/shared/redux/index.js
+++ frontend/src/shared/redux/index.js
@@ -1,12 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-import FavoriteSlice from "./favorite-slice";
 import CartSlice from "./cart-slice";
 
 const store = configureStore({
   reducer: {
     cart: CartSlice.reducer,
-    favorite: FavoriteSlice.reducer,
   },
 });
 
