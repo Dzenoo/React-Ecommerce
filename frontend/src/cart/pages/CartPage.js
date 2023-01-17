a962e35fcfe04fb4a31049549eab91c9f1e85402
Dzenis Gudzevic <110186379+Dzenoo@users.noreply.github.com>
2023-01-17 23:15:59 +0100

    Update CartPage.js

--- frontend/src/cart/pages/CartPage.js
+++ frontend/src/cart/pages/CartPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
 
+
 import Button from "../../shared/components/Form/Button";
 import Card from "../../shared/components/UIelements/Card";
 import CartItem from "../components/CartItem";
