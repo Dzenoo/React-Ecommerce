717bd69c682d2815067a215d4da55dcefd1d9a95
Dzenis Gudzevic <110186379+Dzenoo@users.noreply.github.com>
2023-01-20 18:15:22 +0100

    Update favproduct.js

--- backend/models/favproduct.js
+++ backend/models/favproduct.js
@@ -6,7 +6,7 @@ const favoriteSchema = new Schema({
   title: { type: String, required: true },
   image: { type: String, required: true },
   price: { type: Number, required: true },
-  customer: { type: mongoose.Types.ObjectId, ref: "User", unique: true },
+  customer: { type: mongoose.Types.ObjectId, ref: "User", required: true },
 });
 
 module.exports = mongoose.model("Favorite", favoriteSchema);
